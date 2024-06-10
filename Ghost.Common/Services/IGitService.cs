9d8e19bfa946ae3581489959ad10d817771602d5
isabela.mendes <isabela.mendes@me.com.br>
2024-06-10 16:41:58 -0300

    feat(common): Add Git Service
    This commit introduces a new Git service that provides functionality for interacting with Git repositories. It includes methods for ensuring Git is installed, checking if the current directory is a Git repository, retrieving staged changes, and committing changes.

---
+++ Ghost.Common/Services/IGitService.cs
@@ -0,0 +1,9 @@
+﻿namespace Ghost.Common.Services;
+
+public interface IGitService
+{
+    Task CommitChangesAsync(string message);
+    Task EnsureGitIsInstalledAsync();
+    Task EnsureIsGitRepositoryAsync();
+    Task<string> GetStagedChangesAsync();
+}
---
+++ Ghost.Infrastructure/Services/GitService.cs
@@ -0,0 +1,72 @@
+﻿using CliWrap.Exceptions;
+using CliWrap;
+using Ghost.Common.Services;
+using CliWrap.Buffered;
+
+namespace Ghost.Infrastructure.Services;
+
+public class GitService : IGitService
+{
+    public async Task EnsureGitIsInstalledAsync()
+    {
+        try
+        {
+            var result = await Cli.Wrap("git")
+                .WithArguments("--version")
+                .ExecuteBufferedAsync();
+        }
+        catch (CommandExecutionException ex)
+        {
+            throw new InvalidOperationException("Git is not installed. Please install Git to proceed.", ex);
+        }
+    }
+
+    public async Task EnsureIsGitRepositoryAsync()
+    {
+        try
+        {
+            var result = await Cli.Wrap("git")
+                .WithArguments("rev-parse --is-inside-work-tree")
+                .ExecuteBufferedAsync();
+        }
+        catch (CommandExecutionException ex)
+        {
+            throw new InvalidOperationException("The current directory is not a Git repository.", ex);
+        }
+    }
+
+    public async Task<string> GetStagedChangesAsync()
+    {
+        try
+        {
+            var result = await Cli.Wrap("git")
+                .WithArguments("diff --cached")
+                .ExecuteBufferedAsync();
+
+            if (string.IsNullOrWhiteSpace(result.StandardOutput))
+                throw new InvalidOperationException("No staged changes found.");
+
+            return result.StandardOutput;
+        }
+        catch (CommandExecutionException ex)
+        {
+            throw new InvalidOperationException("Failed to retrieve staged changes from Git, please check your staged changes and try again.", ex);
+        }
+    }
+
+    public async Task CommitChangesAsync(string message)
+    {
+        try
+        {
+            var result = await Cli.Wrap("git")
+                .WithArguments($"commit -m \"{message}\"")
+                .ExecuteBufferedAsync();
+
+            Console.WriteLine(result.StandardOutput);
+        }
+        catch (CommandExecutionException ex)
+        {
+            throw new InvalidOperationException("Git commit failed, please try again", ex);
+        }
+    }
+}
