f9ea8346c48400a845c99899494557a8add405fd
isabela.mendes <isabela.mendes@me.com.br>
2024-06-10 17:02:19 -0300

    feat(prompt): Add prompt model and repository

---
+++ Ghost.Common/Models/Prompt.cs
@@ -0,0 +1,10 @@
+﻿namespace Ghost.Common.Models;
+
+public class Prompt
+{
+    public string Introduction { get; set; } = string.Empty;
+    public string Format { get; set; } = string.Empty;
+    public List<string>? Types { get; set; } = new List<string>();
+    public List<string> Specification { get; set; } = new List<string>();
+    public string? Footer { get; set; } = string.Empty;
+}
---
+++ Ghost.Infrastructure/Persistence/Repositories/PromptRepository.cs
@@ -0,0 +1,21 @@
+﻿using Ghost.Common.Enum;
+using Ghost.Common.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace Ghost.Infrastructure.Persistence.Repositories;
+
+public class PromptRepository
+{
+    private readonly IConfiguration _configuration;
+
+    public PromptRepository(IConfiguration configuration)
+    {
+        this._configuration = configuration;
+    }
+
+    public Prompt GetPromptByName(string templateName)
+    {
+        return _configuration.GetSection($"{nameof(PromptType)}:{templateName}").Get<Prompt>() ??
+            throw new InvalidOperationException($"The template for {templateName} is not configured in appsettings.json.");
+    }
+}
