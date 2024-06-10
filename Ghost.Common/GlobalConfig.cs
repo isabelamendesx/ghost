df969d97b6ed0827acf70ada34c0f268a01c05d6
isabela.mendes <isabela.mendes@me.com.br>
2024-06-10 18:47:39 -0300

    feat(config): Add global configuration class

---
+++ Ghost.Common/GlobalConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace Ghost.Common;
+
+public static class GlobalConfig
+{
+    public static readonly string BaseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+    public static readonly string ConfigFilePath = Path.Combine(BaseDirectory, "appsettings.json");
+}
