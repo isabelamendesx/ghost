b54f13c9f5af68ee53f1e8303ce03c737ea26d47
isabela.mendes <isabela.mendes@me.com.br>
2024-06-16 02:00:59 -0300

    feat(application): Add pre-execution filter

---
+++ Ghost.Application/Filters/ExceptionHandlingFilterAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Cocona.Filters;
+
+namespace Ghost.Application.Filters;
+
+public class ExceptionHandlingFilterAttribute : CommandFilterAttribute
+{
+    public override async ValueTask<int> OnCommandExecutionAsync(CoconaCommandExecutingContext ctx, CommandExecutionDelegate next)
+    {
+        try
+        {
+            return await next(ctx);
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine($"{ex.Message}");
+            return 1;
+        }
+        catch (HttpRequestException)
+        {
+            Console.WriteLine("API request failed. Pleasexxxxxxxxxxxxx check your network connection and try again.");
+            return 1;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"{ex.Message}An unexpected error ocurred");
+            return 1;
+        }
+    }
+}
---
+++ Ghost.Application/Filters/PreExecutionFilter.cs
@@ -0,0 +1,38 @@
+﻿using Cocona.Filters;
+using Ghost.Infrastructure.Services;
+using Library;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Ghost.Application.Filters;
+
+public class PreExecutionFilter : ICommandFilter
+{
+    private readonly IGitService _gitService;
+    private readonly GeminiOptions _geminiOptions;
+
+    public PreExecutionFilter(IOptions<GeminiOptions> geminiOptions, IGitService gitService)
+    {
+        _gitService = gitService;
+        _geminiOptions = geminiOptions.Value;
+    }
+
+    public async ValueTask<int> OnCommandExecutionAsync(CoconaCommandExecutingContext ctx, CommandExecutionDelegate next)
+    {
+        if (string.IsNullOrWhiteSpace(_geminiOptions.ApiKey))
+            throw new InvalidOperationException("API key is not configured. Please set the API_KEY in appsettings.json or type gitghost config apikey");
+
+        await _gitService.EnsureGitIsInstalledAsync();
+        await _gitService.EnsureIsGitRepositoryAsync();
+
+        return await next(ctx);
+    }
+}
+
+class PreExecutionFilterAttribute : Attribute, IFilterFactory
+{
+    public IFilterMetadata CreateInstance(IServiceProvider serviceProvider)
+    {
+        return ActivatorUtilities.CreateInstance<PreExecutionFilter>(serviceProvider);
+    }
+}
