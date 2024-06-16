e9d01b282d2e202d0bd7f1b975cb8f09f48c32be
isabela.mendes <isabela.mendes@me.com.br>
2024-06-16 01:59:52 -0300

    feat(pickMessage): Add message display and selection logic.

---
+++ Ghost.Application/UseCases/PickMessage/PickMessageCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Ghost.Application.UseCases.PickMessage;
+
+public class PickMessageCommand : IRequest<string>
+{
+    public List<string> Messages { get; set; } = [];
+}
---
+++ Ghost.Application/UseCases/PickMessage/PickMessageHandler.cs
@@ -0,0 +1,82 @@
+﻿using MediatR;
+using System.Text;
+
+namespace Ghost.Application.UseCases.PickMessage;
+
+public class PickMessageHandler : IRequestHandler<PickMessageCommand, string>
+{
+    public Task<string> Handle(PickMessageCommand request, CancellationToken cancellationToken)
+    {
+        var messages = request.Messages;
+
+        SetupConsole();
+        DisplayInstructions();
+
+        var selectedOption = HandleUserInput(messages);
+
+        return Task.FromResult(messages.ElementAt(selectedOption));
+    }
+
+    private static void SetupConsole()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.CursorVisible = false;
+    }
+
+    private static void DisplayInstructions()
+    {
+        Console.WriteLine("👻 Pick up a commit message:");
+    }
+
+    private static int HandleUserInput(IEnumerable<string> messages)
+    {
+        (int left, int top) = Console.GetCursorPosition();
+        int selectedOption = 0;
+        const string decorator = "> ";
+        bool isSelected = false;
+
+        while (!isSelected)
+        {
+            Console.SetCursorPosition(left, top);
+            DisplayMessages(messages, selectedOption, decorator);
+
+            ConsoleKeyInfo key = Console.ReadKey(intercept: true);
+            selectedOption = UpdateSelectedOption(key, selectedOption, messages.Count());
+            isSelected = key.Key == ConsoleKey.Enter;
+        }
+
+        return selectedOption;
+    }
+
+    private static void DisplayMessages(IEnumerable<string> messages, int selectedOption, string decorator)
+    {
+        for (int i = 0; i < messages.Count(); i++)
+        {
+            var message = messages.ElementAt(i).Trim();
+            var backgroundColor = BackgroundColors[i % BackgroundColors.Count];
+            var coloredOption = $"{backgroundColor}[{i + 1}]\u001b[0m";
+            var messageColor = selectedOption == i ? "\u001b[90m" : "";
+            Console.WriteLine($"{(selectedOption == i ? decorator : "  ")}{coloredOption} {messageColor}{message}\u001b[0m");
+        }
+    }
+
+    private static int UpdateSelectedOption(ConsoleKeyInfo key, int currentOption, int messageCount)
+    {
+        switch (key.Key)
+        {
+            case ConsoleKey.UpArrow:
+                return currentOption == 0 ? messageCount - 1 : currentOption - 1;
+            case ConsoleKey.DownArrow:
+                return currentOption == messageCount - 1 ? 0 : currentOption + 1;
+            default:
+                return currentOption;
+        }
+    }
+
+    private static readonly List<string> BackgroundColors = new List<string>
+    {
+        "\u001b[48;5;198m\u001b[97m",
+        "\u001b[48;5;210m\u001b[97m",
+        "\u001b[48;5;205m\u001b[97m"
+    };
+}
